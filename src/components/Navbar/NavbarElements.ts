cade212e63958c44ba9700e1e15ed8b704159af6
Adedeji Yusuf <adedejiyusuf26@gmail.com>
2022-05-19 07:39:13 +0100

    Fix

--- src/components/Navbar/NavbarElements.ts
+++ src/components/Navbar/NavbarElements.ts
@@ -65,6 +65,7 @@ export const NavMenu = styled.ul`
     list-style: none;
     text-align: center;
     margin-right: -22px;
+    margin-bottom: 0;
 
     @media screen and (max-width: 768px) {
         display: none;
